3a85a6f2d85dfbd3e8ef0f906891a1ff7042370c
GustavoCGodoy <93145577+GustavoCGodoy@users.noreply.github.com>
2023-11-10 09:43:53 -0300

    Projeto Criado

---
+++ treinamento_funcionarios/src/main/java/com/gustavo/treinamento_funcionarios/TreinamentoFuncionariosApplication.java
@@ -0,0 +1,13 @@
+package com.gustavo.treinamento_funcionarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinamentoFuncionariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinamentoFuncionariosApplication.class, args);
+	}
+
+}
